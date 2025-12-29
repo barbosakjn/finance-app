c5952d51fe3c5fa099ae62ced2caef02620cb654
barbosakjn <87056841+barbosakjn@users.noreply.github.com>
2025-12-29 01:26:07 -0700

    Add /my-jobs route implementation to src/app/my-jobs/page.tsx

---
+++ src/app/my-jobs/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MyJobsPage = () => {
+  return (
+    <div>
+      <h1>My Jobs</h1>
+      <p>Here you can view and manage your jobs.</p>
+    </div>
+  );
+};
+
+export default MyJobsPage;
